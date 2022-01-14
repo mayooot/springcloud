d457f75cf52af9417197d492eebe531162909367
Richard Li <1129972602@qq.com>
2022-01-14 20:03:15 +0800

    Hystrix 熔断

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * <p>项目文档： TODO</p>
+ *
+ * @author liming
+ * @version 1.0.0
+ * @createTime 2022年01月14日 16:53:00
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.atguigu.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -37,8 +40,30 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "  paymentInfo_Timeout, id：" + id + "\t" + "🤖🤖🤖🤖 耗时 "+ timeNumber+" 秒钟";
     }
 
+
     public String paymentInfo_TimeoutHandler(Integer id) {
         return "线程池：" + Thread.currentThread().getName() + "  8001服务器冒烟了，等会再试试吧~, id：" + id + "\t" + "我是背锅的🥵🥵🥵🥵";
 
+    }
+
+    // ***** 服务熔断 *****
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"), // 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), // 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"), // 失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0 ) {
+            throw new RuntimeException("🪲️  ***** id 不能为负数");
+        }
+
+        // IdUtil.simpleUUID() 等价于 UUID.randomUUID().toString()
+        String serialNumber = IdUtil.simpleUUID();
+        return "✔️  " + Thread.currentThread().getName() + "\t" + "调用成功，流水号：" + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "🪲️  " + "id 不能为负数，请稍后再试。 id：" + id;
     }
 }
