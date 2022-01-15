b88be8bd85aaa3818aecdf81e23ca55da778916a
Richard Li <1129972602@qq.com>
2022-01-15 21:00:53 +0800

    add config-test.yml and config-prod.yml

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -2,7 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * <p>项目文档： TODO</p>
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @createTime 2022年01月15日 20:23:00
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableConfigServer
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class, args);
