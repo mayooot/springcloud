a0a0d4af35bb0aee5f2725d7572bab7da687caa3
Richard Li <1129972602@qq.com>
2022-01-16 10:45:41 +0800

    update config.yml

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>项目文档： TODO</p>
+ *
+ * @author liming
+ * @version 1.0.0
+ * @createTime 2022年01月15日 21:32:00
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
